729280003141eabb6837711b6ae8231167eefb0e
Soumil <soumilsarawgi@gmail.com>
2020-05-13 19:36:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mychat/RegisterActivity.java
@@ -0,0 +1,119 @@
+package com.example.mychat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+private Button CreateAccountButton;
+    private EditText UserEmail,UserPassword;
+    private TextView AlreadyHaveAccountLink;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+    private ProgressDialog loadingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        RootRef= FirebaseDatabase.getInstance().getReference();
+
+        mAuth=FirebaseAuth.getInstance();
+        InitializeFields();
+
+        AlreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToLoginActivity();
+            }
+        });
+
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+    }
+
+    private void CreateNewAccount() {
+        String Email= UserEmail.getText().toString();
+        String Password= UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(Email)){
+            Toast.makeText(this, "Please enter Email...", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(Password)){
+            Toast.makeText(this, "Please enter Password...", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please Wait");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+mAuth.createUserWithEmailAndPassword(Email,Password)
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                
+            if(task.isSuccessful()){
+
+                String currentUserId= mAuth.getCurrentUser().getUid();
+                RootRef.child("Users").child(currentUserId).setValue("");
+
+                SendUserToMainActivity();
+                Toast.makeText(RegisterActivity.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+            else{
+                String message= task.getException().toString();
+                Toast.makeText(RegisterActivity.this, "Error!"+message, Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+                
+            }
+            
+        });
+        }
+    }
+
+    private void InitializeFields() {
+        CreateAccountButton=(Button) findViewById(R.id.register_button);
+
+        UserEmail=(EditText) findViewById(R.id.register_email);
+        UserPassword=(EditText) findViewById(R.id.register_password);
+        AlreadyHaveAccountLink=(TextView) findViewById(R.id.already_have_an_account);
+
+loadingBar=new ProgressDialog(this);
+    }
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+}
